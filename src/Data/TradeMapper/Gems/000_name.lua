dde73ac200a85c8b374ad6cad92b78d40d9c440e
justjuangui <servicios@juacarvajal.com>
2024-08-07 10:01:17 -0500

    Gem Transfigure was added in trade mod

--- src/Data/TradeMapper/Gems/000_name.lua
+++ src/Data/TradeMapper/Gems/000_name.lua
@@ -4,10 +4,35 @@ return {
 	run = function (gem, modTrade, parseLineTrade)
 		if gem.nameSpec then
 			local gemName = gem.nameSpec
-			if gem.supportEffect and gem.skillId:match("Support") then
-				gemName = gemName .. " Support"
+			-- validate variant ID
+			if gem.gemData and gem.gemData.variantId:match("Alt[XY]") then
+				local discriminator = ""
+				local option = gem.gemData.variantId:gsub("Alt[XY]", function(k, val)
+					if k == "AltX" then
+						discriminator = "alt_x"
+					elseif k == "AltY" then
+						discriminator = "alt_y"
+					end
+					return ""	
+				end)
+
+				local fmtOption = ""
+				for wrd in option:gmatch("%u%U*") do 
+					fmtOption = fmtOption .. wrd ..  " "
+				end
+
+				fmtOption = fmtOption:match( "^%s*(.-)%s*$" )
+
+				modTrade:NewMod("NameFilter", "type", {
+					discriminator=discriminator,
+					option=fmtOption,
+				})	
+			else
+				if gem.supportEffect and gem.skillId:match("Support") then
+					gemName = gemName .. " Support"
+				end
+				modTrade:NewMod("NameFilter", "type", gemName)
 			end
-			modTrade:NewMod("NameFilter", "type", gemName)
 		end	
 	end
 }

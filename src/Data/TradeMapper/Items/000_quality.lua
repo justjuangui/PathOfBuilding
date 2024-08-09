4c6de150d2332b2073d743031c35e56ce6a9d36c
justjuangui <servicios@juacarvajal.com>
2024-08-08 20:38:01 -0500

    Validate quality greater than 0 was added

--- src/Data/TradeMapper/Items/000_quality.lua
+++ src/Data/TradeMapper/Items/000_quality.lua
@@ -2,7 +2,7 @@ return {
 	id = "QUALITY",
 	name = "Quality",
 	run = function (item, modTrade, parseLineTrade)
-		if item.quality then
+		if item.quality and item.quality > 0 then
 			modTrade:NewMod("MiscFilter", "quality", { min = item.quality })
 		end	
 	end
